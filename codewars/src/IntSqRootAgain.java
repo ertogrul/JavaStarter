2f1a30c2d2eb2ea504efccd13d38595d2cc564fb
Michal Kordyzon <michalkordyzon@gmail.com>
2018-01-18 22:06:12 +0100

    123

--- codewars/src/IntSqRootAgain.java
+++ codewars/src/IntSqRootAgain.java
@@ -1,19 +1,16 @@
-public class IntSqRoot {
+public class  IntSqRootAgain{
     public static long IntRac(long n, long guess)  {
-        int count = 0;
-        long previous_guess;
-        do {
-            System.out.println("count: "+count++);
-            previous_guess = guess;
-            guess = (guess + n / guess) / 2;
-            System.out.println("guess: " + guess);
-            System.out.println("while: " + (guess - previous_guess));
-        } while (Math.abs(guess - previous_guess) >= 1);
-        return count;
+        int counter = 0;
+        for ( long prevGuess = 0; Math.abs(guess - prevGuess) >= 1; counter++ ) {
+            prevGuess = guess;
+            guess = ( guess + n / guess ) / 2;
+        }
+        return counter;
     }
 
+
     public static void main (String[] args) {
-        IntSqRoot pointer = new IntSqRoot();
+        IntSqRootAgain pointer = new IntSqRootAgain();
         //System.out.println(pointer.IntRac(25, 1)); // should return 4;
         //System.out.println(pointer.IntRac(125348,300)); // should return 3;
         //System.out.println(pointer.IntRac(125348981764L,356243)); // should return 3;
---
+++ codewars/src/Movie.java
@@ -0,0 +1,22 @@
+public class Movie {
+
+    public static int movie(int card, int ticket, double perc) {
+        int A;
+        int B;
+        int n = 3;
+        do {
+            A = ticket * n;
+            B = card * ticket * Math.pow(perc, n);
+            System.out.println("A: " + A);
+            System.out.println("B: " + B);
+            n++;
+        } while (n < 100 /*A > B*/);
+        return 10;
+    }
+
+    public static void main (String[] args) {
+        Movie pointer = new Movie();
+        System.out.println(pointer.movie(500, 15, 0.9)); // should return 43
+        //System.out.println(pointer.movie(100, 10, 0.95)); // should return 24
+    }
+}
