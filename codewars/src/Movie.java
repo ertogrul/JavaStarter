68fbae6f4ee575b7e81dd020edf838122ffad02f
Michal Kordyzon <michalkordyzon@gmail.com>
2018-01-19 15:00:46 +0100

    123

--- codewars/src/Movie.java
+++ codewars/src/Movie.java
@@ -1,22 +1,37 @@
 public class Movie {
-
+/*
     public static int movie(int card, int ticket, double perc) {
         int A;
-        int B;
-        int n = 3;
+        double B;
+        int n = 1;
+        double prev_tickets = 0;
         do {
             A = ticket * n;
-            //B = card * ticket * Math.pow(perc, n);
-            System.out.println("A: " + A);
+            B = card + ticket * Math.pow(perc, n) + prev_tickets;
+            prev_tickets += ticket * Math.pow(perc, n);
+            //System.out.println("A: " + A);
             //System.out.println("B: " + B);
             n++;
-        } while (n < 100 /*A > B*/);
-        return 10;
+        } while (A < Math.ceil(B));
+        return n-1;
+    }
+*/
+
+    public static int movie(int card, int ticket, double perc) {
+        int count = 0;
+        double totalB = card;
+
+        while (ticket * count <= Math.ceil(totalB)) {
+            totalB += ticket * Math.pow(perc, ++count);
+        }
+
+        return count;
     }
 
     public static void main (String[] args) {
         Movie pointer = new Movie();
         System.out.println(pointer.movie(500, 15, 0.9)); // should return 43
         //System.out.println(pointer.movie(100, 10, 0.95)); // should return 24
+        //System.out.println(pointer.movie(0, 10, 0.95)); // should return 2
     }
 }
---
+++ codewars/src/PartList.java
@@ -0,0 +1,18 @@
+public class PartList {
+
+    public static String[][] partlist(String[] arr) {
+        String [][] a;
+        System.out.println("Hello");
+
+        return a;
+    }
+
+
+    public static void main (String[] args) {
+        PartList pointer = new PartList();
+        pointer.partlist();
+    }
+
+}
+
+
