042d65248b496a6bd3a1b407b5b88956707a8997
Michal Kordyzon <michalkordyzon@gmail.com>
2018-02-01 13:55:21 +0100

    123

--- codewars/src/StepInPrimes.java
+++ codewars/src/StepInPrimes.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.lang.*;
+
 class StepInPrimes {
 
     public static long[] step(int g, long m, long n) {
@@ -6,58 +8,62 @@ class StepInPrimes {
         // m - search start
         // n - search end
         //
-        // to do:
-        // - list primes
-        // - look for way to count steps between them
-        //
-        long[] firstPair = new long [2];
-        //long[] tempArr = new long [10];
         ArrayList tempArr = new ArrayList();
+        int counter = 0;
+        long [] arr = {0, 0};
+        boolean resultFound = false;
 
-        for (int i = 3; i <= n; i++) {
+        if (m == n) return new long [] {};
+
+        for (long i = m; i <= n; i++) {
             if (isPrime(i) == true) {
-                //System.out.println(i);
                 tempArr.add(i);
                 System.out.println("tempArr: " + tempArr);
-                if (tempArr.size() > 1) {
-                    for (int z = 0; z < tempArr.size(); z++) {
-                        System.out.println("XXXX "+tempArr.get(z));
-
-                        int a = (int) tempArr.get(z);
-                        //ArrayList b = tempArr.get(z-1);
-                        /*if (tempArr.get(z) - tempArr.get(z - 1) == g) {
-                            firstPair[0] = z;
-                            firstPair[1] = i;
-                            break;
-                        }*/
-                    }
-
-                }
-
             }
+        }
+
+        for (int j = 0; j < tempArr.size(); j++) {
+            for (int shiftVar = 1; shiftVar < tempArr.size() - counter; shiftVar++) {
+                //long temp = (long) tempArr.get(j+shiftVar) - (long) tempArr.get(j);
+                //if (g == ((int) tempArr.get(j+shiftVar) - (int) tempArr.get(j))) {
+                if (resultFound == true) break;
+
+                long temp2 =  (long) tempArr.get(j+shiftVar) - (long) tempArr.get(j);
+                if (temp2 == g) {
+                    arr[0] = (long) tempArr.get(j);
+                    arr[1] = (long) tempArr.get(j+shiftVar);
+                    System.out.println("KONIEC");
+                    for (long d : arr) System.out.println(d);
+                    resultFound = true;
+                    break;
+                }
+            }
+            counter++;
 
         }
 
-        return firstPair;
+        if (arr[1] == 0) return null;
+        return arr;
     }
 
     public static boolean isPrime(long n) {
         if (n % 2 == 0) return false;
-        for(int i = 3; i * i <= n; i += 2) {
-            if(n % i == 0) return false;
+        for(int a = 3; a * a <= n; a += 2) {
+            //System.out.println(a);
+            if(n % a == 0) return false;
         }
         return true;
     }
 
     public static void main (String[] args) {
-        StepInPrimes pointer = new StepInPrimes();
-        System.out.println(pointer.step(2, 2, 50)); // [3,5]
-        // pointer.step(2, 5, 7); // [5, 7]
-        // pointer.step(2, 5, 5); // [ ]
-        // pointer.step(2,100,110); // [101, 103]
-        // pointer.step(4, 100, 110); // [103, 107]
-        // pointer.step(6, 100, 110); // [101, 107]
-        // pointer.step(8, 300, 400); // [359, 367]
-        // pointer.step(10, 300, 400); // [307, 317]
+        // StepInPrimes pointer = new StepInPrimes();
+        // StepInPrimes.step(2, 2, 50); // [3,5]
+        // StepInPrimes.step(2, 5, 7); // [5, 7]
+        // StepInPrimes.step(2, 5, 5); // [ ]
+        // StepInPrimes.step(2,100,110); // [101, 103]
+        // StepInPrimes.step(4, 100, 110); // [103, 107]
+        // StepInPrimes.step(6, 100, 110); // [101, 107]
+        // StepInPrimes.step(8, 300, 400); // [359, 367]
+        // StepInPrimes.step(10, 300, 400); // [307, 317]
     }
 }
